9eebc6aaf237e5fae2bf1d0473d6bf24e56bb4ee
Erik Sales <eriksales1@hotmail.com>
2023-01-05 11:40:54 -0500

    first JS commit, const questions

---
+++ main.js
@@ -0,0 +1,54 @@
+const questions = [
+    {
+        text: 'How many provinces are there in Canada?',
+        options:
+         [
+                '10 provinces',
+                '13 provinces',
+                '12 provinces',
+                '10 provinces and 3 territories'
+          ],
+        answer: '10 provinces and 3 territories'
+
+    },
+
+    {
+        text: 'What is the name of the Capital of Canada?',
+        options:
+         [
+                'Vancouver',
+                'Ottawa',
+                'Toronto',
+                'Montreal'
+          ],
+        answer: 'Toronto'
+
+    },
+
+    {
+        text: 'What is the most popular sport in Canada?',
+        options:
+         [
+                'Skiig',
+                'Snowboard',
+                'Baseball',
+                'Hockey'
+          ],
+        answer: 'Hockey'
+
+    },
+
+    {
+        text: 'What is the coldest ever recorded temperature in Canada?',
+        options:
+         [
+                '-63°C',
+                '-75°C',
+                '-51°C',
+                '-45°C'
+          ],
+        answer: '-63°C'
+
+    },
+
+]
